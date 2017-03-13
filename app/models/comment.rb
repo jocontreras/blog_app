204b3ec79aa350b8c973e69bca9a8d0499c18205
jocontreras <jen.almarasy@gmail.com>
2017-03-13 09:50:16 +0100

    fix test failed with the email validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ApplicationRecord
   validates :body, presence: true
   validates :article_id, presence: true
 
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :show}
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create}
 
 
 end
--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :comment do
+    email "jen.almarasy@gmail.com"
     body "MyText"
     article_id 1
   end
