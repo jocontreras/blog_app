d88b357c4a027dfeb047d6772e50a4231fc824d2
jocontreras <jen.almarasy@gmail.com>
2017-03-13 10:01:54 +0100

    fix validation test-writing comment without email

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,8 @@ class Comment < ApplicationRecord
   validates :body, presence: true
   validates :article_id, presence: true
 
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create}
-
+  unless @email.nil?
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create}
+  end
 
 end
